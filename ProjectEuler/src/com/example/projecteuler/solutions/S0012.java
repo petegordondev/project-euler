47779bdb7b475216db8fa25505ef2652215766e0
Pete Gordon <peter.gordon87@gmail.com>
2016-05-02 16:30:05 +0100

    Add TriangularHelper

---
+++ ProjectEuler/src/com/example/projecteuler/solutions/S0012.java
@@ -0,0 +1,20 @@
+package com.example.projecteuler.solutions;
+
+import com.example.projecteuler.Solution;
+import com.example.projecteuler.utils.TriangularHelper;
+
+import java.util.List;
+
+public class S0012 implements Solution {
+    @Override
+    public String getAnswer() {
+        return Long.toString(originalSolution());
+    }
+
+    private long originalSolution() {
+
+        TriangularHelper th = new TriangularHelper();
+
+        return 0;
+    }
+}
---
+++ ProjectEuler/src/com/example/projecteuler/utils/TriangularHelper.java
@@ -0,0 +1,39 @@
+package com.example.projecteuler.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TriangularHelper {
+
+    private List<Integer> list = new ArrayList<>();
+
+    public TriangularHelper() {
+        list.add(1);
+    }
+
+    public int get(int n){
+        if (getCount() < n) generateBatch(n);
+        return list.get(n - 1);
+    }
+
+    public List<Integer> getList(int n){
+        generateBatch(n);
+        return list;
+    }
+
+    private int getCount(){
+        return list.size();
+    }
+
+    private void generateBatch(int n){
+        while (getCount() < n) generateNext();
+    }
+
+    private void generateNext(){
+        // Get latest in list.
+        int latest = list.get(getCount() - 1);
+        // Add to latest.
+        latest += getCount() + 1;
+        list.add(latest);
+    }
+}
