a1f3986cda506809ddcdd9b92971aa1e372c902e
Pete Gordon <peter.gordon87@gmail.com>
2016-08-16 20:45:02 +0100

    Start sol 27.

--- ProjectEuler/src/com/example/projecteuler/solutions/S0027.java
+++ ProjectEuler/src/com/example/projecteuler/solutions/S0027.java
@@ -4,6 +4,10 @@ import com.example.projecteuler.Solution;
 import com.example.projecteuler.utils.PrimeHelper;
 
 public class S0027 implements Solution {
+    private int target = 1000;
+    private int ans;
+    private int numPrimes = 0;
+
     @Override
     public String getAnswer()  {
         return Integer.toString(original_solution());
@@ -11,12 +15,28 @@ public class S0027 implements Solution {
 
     private int original_solution() {
 
-        int res = 0;
+        for(int a = -target; a <= target; a++){
+            for(int b = -target; b <= target; b++){
+                int n = 1;
+                int num = 0;
+                int res = (n*n) + (a*n) + (b*n);
 
-for (int i = 2; i < 100; i++){
-    System.out.println(i + "\t" + new PrimeHelper().isPrime(i));
-}
+                //Find how many primes there are.
+                if (res > 1){
+                    while (new PrimeHelper().isPrime(res)){
+                        res = (n*n) + (a*n) + (b*n);
+                        num++;
+                        n++;
+                    }
 
-        return 0;
+                    if (numPrimes < num){
+                        numPrimes = num;
+                        ans = a * b;
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
 }
