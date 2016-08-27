088510730c10f534edfaea939a7a72cac61a5667
Pete Gordon <peter.gordon87@gmail.com>
2016-08-27 18:25:20 +0100

    Add solution 27.

--- ProjectEuler/src/com/example/projecteuler/solutions/S0027.java
+++ ProjectEuler/src/com/example/projecteuler/solutions/S0027.java
@@ -3,6 +3,8 @@ package com.example.projecteuler.solutions;
 import com.example.projecteuler.Solution;
 import com.example.projecteuler.utils.PrimeHelper;
 
+import java.util.List;
+
 public class S0027 implements Solution {
     private int target = 1000;
     private int ans;
@@ -15,28 +17,37 @@ public class S0027 implements Solution {
 
     private int original_solution() {
 
-//        for(int a = -target; a <= target; a++){
-//            for(int b = -target; b <= target; b++){
-//                int n = 1;
-//                int num = 0;
-//                int res = (n*n) + (a*n) + (b*n);
-//
-//                //Find how many primes there are.
-//                if (res > 1){
-//                    while (new PrimeHelper().isPrime(res)){
-//                        res = (n*n) + (a*n) + (b*n);
-//                        num++;
-//                        n++;
-//                    }
-//
-//                    if (numPrimes < num){
-//                        numPrimes = num;
-//                        ans = a * b;
-//                    }
-//                }
-//            }
-//        }
+        // Get all primes under and equal to 1000; b has to be one of these.
+        List<Integer> primesForB = PrimeHelper.getInstance().findPrimesUnder(target);
 
+        for (Integer prime : primesForB) {
+            // b must be positive, otherwise result for n = 0 will be negative and therefore not a prime.
+            int b = prime;
+
+            // Lower limits of a are determined at n = 1 (result must be 2).
+            int aLow = -1 * (b + 1);
+
+            for (int a = aLow; a < target; a++) {
+
+                // Reset and get first result.
+                int n = 0;
+                int num = 0;
+                int res = (n * n) + (a * n) + b;
+
+                //Find how many primes there are.
+                while (PrimeHelper.getInstance().isPrime(res)) {
+                    num++;
+                    n++;
+                    res = (n * n) + (a * n) + b;
+                }
+
+                if (numPrimes < num) {
+                    numPrimes = num;
+                    ans = a * b;
+                }
+
+            }
+        }
         return ans;
     }
 }
